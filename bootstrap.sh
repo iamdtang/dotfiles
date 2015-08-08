cd26a454cc01bbc9d53af47c2a7d51aa7ce84af1
David Tang <dtang859@gmail.com>
2015-08-08 11:46:06 -0700

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,4 +3,6 @@ current_npm_prefix = npm config get prefix
 echo "Changing npm prefix to $current_npm_prefix"
 npm config set prefix ~/npm
 
+cp .bash_profile ~/.bash_profile
+
 source ~/.bash_profile;
